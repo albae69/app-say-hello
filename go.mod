4727831f658ed9b50155ad215df9e99fc87a0de8
albae69 <ahmadalbaihaqi69@gmail.com>
2023-10-31 17:31:03 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/albae69/app-say-hello
+
+go 1.21.0
+
+require github.com/albae69/learn-go-modules/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	learn_go_modules "github.com/albae69/learn-go-modules/v2"
+)
+
+func main() {
+	fmt.Println(learn_go_modules.SayHello())
+}
